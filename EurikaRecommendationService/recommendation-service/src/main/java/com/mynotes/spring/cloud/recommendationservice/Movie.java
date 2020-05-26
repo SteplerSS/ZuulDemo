e37e6da319c63b5fc3fae80ffa37f7eee804bf7a
S.Rodionov <SNRodionovv@gmail.com>
2020-05-25 23:57:13 -0500

    05/25/2020 #1

--- EurikaRecommendationService/recommendation-service/src/main/java/com/mynotes/spring/cloud/recommendationservice/Movie.java
+++ EurikaRecommendationService/recommendation-service/src/main/java/com/mynotes/spring/cloud/recommendationservice/Movie.java
@@ -7,9 +7,30 @@ public class Movie {
 
     private String synopsis;
 
-    public Movie(Integer id, String name, String synopsis) {
+    public Movie() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
         this.synopsis = synopsis;
     }
 
